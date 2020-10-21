ed140580d0a018f7bf8bf3ba8f33660b506323e4
Stacy Burris <stacy1burris@gmail.com>
2020-10-21 11:14:44 -0700

    finished javascript for celebrity login

--- celebritylogin-app.js
+++ celebritylogin-app.js
@@ -1,33 +1,34 @@
 'use strict';
 
-var numberOfTrys = 3;
+var numberOfTries = 3;
 var numberOfAttempts = 0;
 
 var listenerButton = document.getElementById('celebrityLoginButton');
-listenerButton.addEventListener('click', handleClick);
+
 
 function handleClick(event) {
-    if (numberOfAttempts < numberOfTrys) {
-        alert('Incorrect Password: Please Try Again');
+  event.preventDefault();
+  numberOfAttempts++;
+  if (numberOfAttempts < numberOfTries) {
+    alert('Incorrect Password: Please Try Again');
 
-    }
+  } else if (numberOfAttempts === numberOfTries) {
+    alert('Too Many Attempts, Please Reset Password');
 
+  }
 }
 
-numberOfAttempts++;
-    
-}
 
-
+listenerButton.addEventListener('click', handleClick);
 
 // number of attempts
 // alert / prompt please reset username password
 // allow to try 3-5 trys to get password
 
-// list3en for click on login button 
+// list3en for click on login button
 // response that it is incorrect no matter what they enter
 
 // hit login and told incorrect
 //security questionn/password reset required prompt
-// directs to security page once prompt is told  
+// directs to security page once prompt is told
 
