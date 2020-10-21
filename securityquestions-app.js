3b9101b2fd9811d47cbe1cbe6cc83664e79178c1
Michael Falk <falk478@yahoo.com>
2020-10-21 15:40:44 -0700

    added question check to all three  personal questions

--- securityquestions-app.js
+++ securityquestions-app.js
@@ -2,27 +2,28 @@
 
 var correctAnswerNumber = 0;
 var correctAnswerOne = 'Brooklyn';
+var correctAnswerTwo = 'Wilson';
+var correctAnswerThree = 'Ice Cream Truck';
 var attemptLimit = 3;
 var attemptsSoFar = 0;
 
-var securityQuestionOneElement = document.getElementById('firstAnswer');
-securityQuestionOneElement.addEventListener('click', handleClick);
-function handleClick(event) {
-  event.preventDefault();
-  console.log(event.target);
-  var userInputOne = document.getElementById('firstInput').value;
-  console.log(userInputOne);
-
-  if (correctAnswerOne === userInputOne) {
-    correctAnswerNumber++;
-    correctAttempt();
-    // if correct do this
-    //if incorrect do this other thing
-  } else {
-    incorrectAttempt();
+function questionOne(){
+  var securityQuestionOneElement = document.getElementById('firstAnswer');
+  securityQuestionOneElement.addEventListener('click', handleClick);
+  function handleClick(event) {
+    event.preventDefault();
+    console.log(event.target);
+    var userInputOne = document.getElementById('firstInput').value;
+    console.log(userInputOne);
+    if (correctAnswerOne === userInputOne) {
+      correctAnswerNumber++;
+      correctAttempt();
+    } else {
+      incorrectAttempt();
+    }
   }
 }
-
+questionOne();
 
 function correctAttempt(){
   var correctText = document.createElement('p');
@@ -37,6 +38,76 @@ function incorrectAttempt(){
   var parentElementQOne = document.getElementById('firstInput');
   incorrectText.textContent='Incorrect Guess';
   parentElementQOne.after(incorrectText);
+}
+
+
+
+
+function questionTwo(){
+  var securityQuestionTwoElement = document.getElementById('secondAnswer');
+  securityQuestionTwoElement.addEventListener('click', handleClick);
+  function handleClick(event) {
+    event.preventDefault();
+    console.log(event.target);
+    var userInputTwo = document.getElementById('secondInput').value;
+    console.log(userInputTwo);
+    if (correctAnswerTwo === userInputTwo) {
+      correctAnswerNumber++;
+      correctAttemptTwo();
+    } else {
+      incorrectAttemptTwo();
+    }
+  }
+}
+questionTwo();
+
+function correctAttemptTwo(){
+  var correctText = document.createElement('p');
+  var parentElementQTwo = document.getElementById('secondInput');
+  correctText.textContent ='Correct!';
+  parentElementQTwo.after(correctText);
+
+}
+
+function incorrectAttemptTwo(){
+  var incorrectText = document.createElement('p');
+  var parentElementQTwo = document.getElementById('secondInput');
+  incorrectText.textContent='Incorrect Guess';
+  parentElementQTwo.after(incorrectText);
+}
+
+
+function questionThree(){
+  var securityQuestionThreeElement = document.getElementById('thirdAnswer');
+  securityQuestionThreeElement.addEventListener('click', handleClick);
+  function handleClick(event) {
+    event.preventDefault();
+    console.log(event.target);
+    var userInputThree = document.getElementById('thirdInput').value;
+    console.log(userInputThree);
+    if (correctAnswerThree === userInputThree) {
+      correctAnswerNumber++;
+      correctAttemptThree();
+    } else {
+      incorrectAttemptThree();
+    }
+  }
+}
+questionThree();
+
+function correctAttemptThree(){
+  var correctText = document.createElement('p');
+  var parentElementQThree = document.getElementById('thirdInput');
+  correctText.textContent ='Correct!';
+  parentElementQThree.after(correctText);
+
+}
+
+function incorrectAttemptThree(){
+  var incorrectText = document.createElement('p');
+  var parentElementQThree = document.getElementById('thirdInput');
+  incorrectText.textContent='Incorrect Guess';
+  parentElementQThree.after(incorrectText);
 }
 // select security question answer based off quick google searches
 // x amount of tries if ita wrong
