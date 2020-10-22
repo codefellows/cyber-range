df16fce4b734141840376bd63a951b3161f4566e
Stacy Burris <stacy1burris@gmail.com>
2020-10-22 12:46:11 -0700

    got functionality to work

--- celebritylogin-app.js
+++ celebritylogin-app.js
@@ -2,24 +2,67 @@
 
 var numberOfTries = 3;
 var numberOfAttempts = 0;
+var correctPassword = 'donny';
+var button = document.getElementById('login-box');
 
-var listenerButton = document.getElementById('celebrityLoginButton');
 
+//need to add collect text box entree
 
 function handleClick(event) {
   event.preventDefault();
-  numberOfAttempts++;
-  if (numberOfAttempts < numberOfTries) {
-    alert('Incorrect Password: Please Try Again');
 
-  } else if (numberOfAttempts === numberOfTries) {
-    alert('Too Many Attempts, Please Reset Password');
+  var attemptedPassword = event.target.passwordone.value;
+
+  console.log(event.target);
+  numberOfAttempts++;
+  console.log('event whats happening', attemptedPassword, correctPassword);
+  if (attemptedPassword === correctPassword) {
+    correctPasswordEntered();
+
+  } else if (numberOfAttempts < numberOfTries) {
+    incorrectPassword();
 
   }
+  if (numberOfAttempts === 3) {
+
+    location.href = 'securityquestions.html';
+
+  }
+
 }
 
 
-listenerButton.addEventListener('click', handleClick);
+button.addEventListener('submit', handleClick);
+
+
+function correctPasswordEntered() {
+  console.log('the correct password was entered');
+
+  location.href = 'celebrity-hack-sb-photos.html';
+}
+
+
+function incorrectPassword() {
+  console.log('is there a incorrect password?');
+
+  let modalBtn = document.getElementById('modal-btn');
+  let modal = document.querySelector('.modal');
+  let closeBtn = document.querySelector('.close-btn');
+  modalBtn.onclick = function () {
+    modal.style.display = 'block';
+  };
+  closeBtn.onclick = function () {
+    modal.style.display = 'none';
+  };
+  window.onclick = function (e) {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+    }
+  };
+}
+// var finalSubmit = document.getElementById()
+
+
 
 // number of attempts
 // alert / prompt please reset username password
