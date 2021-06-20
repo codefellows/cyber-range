b4355012f99c4c8b4759ad4ab731ddec5fac14fb
Alex Angelico <alex.angelico@live.com>
2021-06-19 19:08:55 -0700

    changed var to let; other minor refactoring

--- acmerocketco/js/corporate-espionage.js
+++ acmerocketco/js/corporate-espionage.js
@@ -1,28 +1,25 @@
 // alert('Do you have what it takes to hack the Acme Rocket Co. store front? You\'ve heard rumors they are working on a new model called the Javelin Rocket Model b Series 3 the details are set to be released in the next month. Do you think you can hack their site and get the scoop before anyone else does?');
 
-function instructionsForHacking(){
-  var updateButon = document.getElementById('updateDetails');
-  var cancelButton = document.getElementById('cancel');
-  var dialog = document.getElementById('favDialog');
+function instructionsForHacking() {
+  let updateButon = document.getElementById('updateDetails');
+  let cancelButton = document.getElementById('cancel');
+  let dialog = document.getElementById('favDialog');
   dialog.returnValue = 'favAnimal';
 
-  function openCheck(dialog){
-    if(dialog.open){
-      console.log('Dialog open');
-    } else {
-      console.log('Dailog closed');
-    }
+  function openCheck(dialog) {
+    if (dialog.open) { console.log('Dialog open'); }
+    else { console.log('Dailog closed'); }
   }
   //help button opens a modal
-  updateButon.addEventListener('click',function(){
+  updateButon.addEventListener('click', function () {
     dialog.showModal();
     openCheck(dialog);
   });
   //add cancel button to close modal dialog box
-  cancelButton.addEventListener('click',function(){
+  cancelButton.addEventListener('click', function () {
     dialog.closest('animalNotChosen');
     openCheck(dialog);
   });
-
 }
+
 instructionsForHacking();
